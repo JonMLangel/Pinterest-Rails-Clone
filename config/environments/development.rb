79c55b6eb2c0e2d0ff79b345f39f8a92509a39a8
JonMLangel <JonLangel@midwestmaid.com>
2016-09-13 18:46:51 -0500

    Add image uploading with Paperclip and ImageMagick

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,6 +51,11 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+Paperclip.options[:command_path] = 'C:\Program Files (x86)\GnuWin32\bin'
+
+config.serve_static_assets = true
+
+
 config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
 # Used for devise Gem
